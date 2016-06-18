722a339a34533446e7930fa18432d6a183cfecb6
laps <liaopanpan610@pingan.com.cn>
2016-06-18 16:25:23 +0800

    add p2p

---
+++ app/src/main/java/com/amenuo/monitor/activity/MainActivity.java
@@ -0,0 +1,36 @@
+package com.amenuo.monitor.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
+import android.widget.Toast;
+
+
+import com.amenuo.monitor.R;
+import com.amenuo.monitor.actions.TwiceBack;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TwiceBack mTwiceBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTwiceBack = new TwiceBack();
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_BACK){
+            mTwiceBack.backPress();
+            if (!mTwiceBack.canBack()){
+                Toast.makeText(this, "再按一次退出", Toast.LENGTH_SHORT).show();
+                return false;
+            }
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+}
