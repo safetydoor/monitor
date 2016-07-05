60297b8cd6fc05cf2d200ed92f85611749188dae
laps <liaopanpan610@pingan.com.cn>
2016-07-05 12:48:03 +0800

    COPY

---
+++ app/src/main/java/com/amenuo/activity/VideoPlayerActivity.java
@@ -0,0 +1,15 @@
+package com.amenuo.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.amenuo.yoosee.R;
+
+public class VideoPlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_player);
+    }
+}
